4a568cb110d0a2dcd4245017b6be6b80a2157310
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-01 23:37:37 +0100

    fix: merge conflicts

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -6,17 +6,17 @@ The modern Lavalink wrapper designed for discord.py.
 :copyright: 2021, cloudwithax
 :license: GPL-3.0
 """
-import discord
+#import discord
 
-if not discord.__version__.startswith("2.0"):
-    class DiscordPyOutdated(Exception):
-        pass
+#if not discord.__version__.startswith("2.0"):
+#    class DiscordPyOutdated(Exception):
+#        pass
 
-    raise DiscordPyOutdated(
-        "You must have discord.py 2.0 to use this library. "
-        "Uninstall your current version and install discord.py 2.0 "
-        "using 'pip install git+https://github.com/Rapptz/discord.py@master'"
-    )
+#    raise DiscordPyOutdated(
+#        "You must have discord.py 2.0 to use this library. "
+#        "Uninstall your current version and install discord.py 2.0 "
+#        "using 'pip install git+https://github.com/Rapptz/discord.py@master'"
+#    )
 
 __version__ = "1.1.7b"
 __title__ = "pomice"
